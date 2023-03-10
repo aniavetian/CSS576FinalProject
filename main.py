176f4a75a9ed01ec01deb381bf0a4ed8c48587a9
avetian7 <avetian7@uw.edu>
2023-03-09 19:14:38 -0800

    create model code added

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
-
+from model import Model
 
 def main():
-    print("Hello World!")
+   model = Model()
+
+   model.load_dataset('COMBINEDFINAL.csv')
+   model.prep_dataset()
+   model.neural_network()
+
+   # Can use my_model with the predict function
+   my_model = model.get_model()
+
 
 
 if __name__ == '__main__':
