85daae0747b1f143ddffe93a237eb59456819dfe
Christian Bergh <cbergh314@gmail.com>
2023-03-10 23:19:37 -0800

    refactor model to simplify constructor and use private methods

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from model import Model
 import numpy as np
 
-def main():
-    model = Model()
 
-    model.load_dataset('COMBINEDFINAL.csv')
-    model.prep_dataset()
-    model.neural_network()
+def main():
+    model = Model('COMBINEDFINAL.csv')
 
     # Can use my_model with the predict function
     my_model = model.get_model()
 
-    x = np.array([[0.0, 0.44, 0.001, 0.8, 0.75]])
+    x = np.array([[0.0, 0.44, 0.001, 0.8, 0.75]])  # Test Value to test prediction
     result = my_model.predict(x)
-    print(result)
+    print(result)  # test prediction
+
+    result2 = model.model.predict(x)
+    print(result2)
+
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -10,93 +10,100 @@ File contains the code to build the neural network
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-import numpy as np
 from keras.layers import Dense, Dropout
 from keras import models
 
+
 class Model:
-   """
+    """
    Initializer
    """
-   def __init__(self):
-      self.model = None
-      self.df = None
-      self.x_train = None
-      self.x_test = None
-      self.y_train = None
-      self.y_test = None
-      self.UNIT = 5
-      self.EPOCHS = 200
-      self.BATCH_SIZE = 100
 
-   """
-   Pass in a file name to load in the dataset to be used to train the model
-   """
-   def load_dataset(self, file_name):
-      self.df = pd.read_csv(file_name)
+    def __init__(self, file_name):
+        self.model = None
+        self.df = None
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
+        self.UNIT = 5
+        self.EPOCHS = 200
+        self.BATCH_SIZE = 100
 
-   """
-   Custom encoding function to encode whether we have spam or ham in the dataset
-   """
-   def custom_encode(self, val):
-      if val == 'spam':
-         return 1
-      else:
-         return 0
+        self.__load_dataset(file_name)
+        self.__prep_dataset()
+        self.__neural_network()
 
-   """
-   Function prepares the data to be used by the neural network
-   """
-   def prep_dataset(self):
+    def __load_dataset(self, file_name):
+        """
+        Pass in a file name to load in the dataset to be used to train the model
+        :param file_name:
+        """
+        self.df = pd.read_csv(file_name)
 
-      # Only using relevent columns with features we want
-      self.df = self.df[['non_standard_html_tag_count', 'spam_keywords', 'total_links', 'total_keywords_in_subject', 'total_keywords_in_body', 'classification']]
+    def custom_encode(self, val):
+        """
+        Custom encoding function to encode whether we have spam or ham in the dataset
+        """
+        if val == 'spam':
+            return 1
+        else:
+            return 0
 
-      # Scale numerical columns (normalize) so model can understand them more easily
-      scaler = MinMaxScaler()
-      self.df[['non_standard_html_tag_count', 'spam_keywords', 'total_links', 'total_keywords_in_subject', 'total_keywords_in_body']] = \
-         scaler.fit_transform(self.df[['non_standard_html_tag_count', 'spam_keywords', 'total_links', 'total_keywords_in_subject', 'total_keywords_in_body']])
+    def __prep_dataset(self):
+        """
+        Function prepares the data to be used by the neural network
+        """
+        # Only using relevant columns with features we want
+        self.df = self.df[['non_standard_html_tag_count', 'spam_keywords', 'total_links', 'total_keywords_in_subject',
+                           'total_keywords_in_body', 'classification']]
 
-      self.df['classification'] = self.df['classification'].apply(self.custom_encode)
+        # Scale numerical columns (normalize) so model can understand them more easily
+        scaler = MinMaxScaler()
+        self.df[['non_standard_html_tag_count', 'spam_keywords', 'total_links', 'total_keywords_in_subject',
+                 'total_keywords_in_body']] = \
+            scaler.fit_transform(self.df[['non_standard_html_tag_count', 'spam_keywords', 'total_links',
+                                          'total_keywords_in_subject', 'total_keywords_in_body']])
 
-      # Seperate into features (X) and lable (y)
-      X = self.df.loc[:, self.df.columns != 'classification']
-      y = self.df['classification']
+        self.df['classification'] = self.df['classification'].apply(self.custom_encode)
 
-      # Do a 75% (data) to 25% (test data) split
-      self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(X, y, random_state=104, test_size=0.25, shuffle=True)
+        # Separate into features (X) and label (y)
+        X = self.df.loc[:, self.df.columns != 'classification']
+        y = self.df['classification']
 
+        # Do a 75% (data) to 25% (test data) split
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(X, y, random_state=104, test_size=0.25,
+                                                                                shuffle=True)
 
-   """
-   Function build the neural network and uses early stopping to ensure we prevent
-   overfitting
-   """
-   def neural_network(self):
+    def __neural_network(self):
+        """
+        Function build the neural network and uses early stopping to ensure we prevent overfitting
+        """
+        # Build neural network
+        self.model = models.Sequential()
+        self.model.add(Dense(self.UNIT, activation='relu'))
+        self.model.add(Dropout(0.15))
+        self.model.add(Dense(self.UNIT, activation='relu'))
+        self.model.add(Dropout(0.05))
+        self.model.add(Dense(1, activation='sigmoid'))
 
-      # Build neural network
-      self.model = models.Sequential()
-      self.model.add(Dense(self.UNIT, activation='relu'))
-      self.model.add(Dropout(0.15))
-      self.model.add(Dense(self.UNIT, activation='relu'))
-      self.model.add(Dropout(0.05))
-      self.model.add(Dense(1, activation='sigmoid'))
+        # Compile model
+        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-      # Compile model
-      self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        # Train model + use early stopping
+        self.model.fit(self.x_train, self.y_train, batch_size=self.BATCH_SIZE, epochs=self.EPOCHS,
+                       validation_data=(self.x_test, self.y_test))
 
-      # Train model + use early stopping
-      self.model.fit(self.x_train, self.y_train, batch_size=self.BATCH_SIZE, epochs=self.EPOCHS, validation_data=(self.x_test, self.y_test))
+        # Evaluate model
+        score = self.model.evaluate(self.x_test, self.y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
 
-      # Evaluate model
-      score = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-      print('Test loss:', score[0])
-      print('Test accuracy:', score[1])
+        # Summary of neural network
+        self.model.summary()
 
-      # Summary of neural network
-      self.model.summary()
-
-   """
-   Return the neural network model for later use.
-   """
-   def get_model(self):
-      return self.model
+    def get_model(self):
+        """
+        Return the neural network model for later use.
+        """
+        return self.model
